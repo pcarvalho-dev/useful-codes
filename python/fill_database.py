ea40e0296d04a84eca323758146619fea554f320
Pablo Carvalho <pablocarvalho.dev@gmail.com>
2022-09-12 13:15:08 -0300

    first commit

---
+++ python/fill_database.py
@@ -0,0 +1,39 @@
+import json
+import random
+
+import requests
+from faker import Faker
+
+faker = Faker()
+
+
+def fill_database(quantity):
+    """This script fills a database table
+
+    Args:
+        quantity (int): The quantity of registers to insert in database
+    """
+    for i in range(0, total):
+        url = "http://localhost:4130/v1/client"
+        headers = {
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "life_style": faker.text(),
+            "birth_date": faker.date(),
+            "status": True,
+            "genre": random.choice(["male", "female"]),
+            "password": "123456",
+            "taxpayer": faker.random_number(digits=10),
+            "cell_phone": faker.phone_number(),
+            "interests": random.choice(["male", "female"]),
+            "sexual_orientation": random.choice(["male", "female"]),
+            "email": faker.email(),
+            "bio": faker.text(),
+            "name": faker.name()
+        }
+        r = requests.post(url, headers=headers, data=json.dumps(payload))
+        print(r.text)
+
+
+fill_database(15)
